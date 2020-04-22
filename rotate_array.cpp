4c9f3e9d5017b456ac6a789b3f00f76ab6973b15
Mahima gupta <54117469+mahima-c@users.noreply.github.com>
2020-04-22 12:11:28 +0530

    Create rotate_array.cpp

---
+++ rotate_array.cpp
@@ -0,0 +1,2 @@
+//Given a random integer array of size n, write a function that rotates the given array by d elements (towards left)
+//Change in the input array itself. You don't need to return or print elements.
