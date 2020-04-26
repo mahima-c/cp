2f57ecc9706ab91f5fcde33eab4aa7f320f48500
Mahima gupta <54117469+mahima-c@users.noreply.github.com>
2020-04-26 21:34:41 +0530

    Create recursion.cpp

---
+++ recursion.cpp
@@ -0,0 +1,5 @@
+//break problem to smallest problem which can solve
+when we solve a problem by recursion then contionusly call a fuction when its not reaches base condition
+then fuction is pause and wait to return vale of below after that it return above function
+//fuction is as local var in recursion 
+//recursion work on PMI(principle of mathmetical induction
